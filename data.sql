8df720573aa2ad4a55dc6a0286981fab88edad84
Jonathan Jin <jjin082693@gmail.com>
2014-04-09 16:19:13 -0500

    rm placeholder weapon vals

--- data.sql
+++ data.sql
@@ -1,8 +1,8 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE weapons(name, valMin, valMax);
-INSERT INTO "weapons" VALUES('Twig',1,5);
 CREATE TABLE armor(name, type, valMin, valMax);
+INSERT INTO "weapons" VALUES('Twig',NULL,NULL);
 INSERT INTO "weapons" VALUES('Wok',NULL,NULL);
 INSERT INTO "weapons" VALUES('Staff',NULL,NULL);
 COMMIT;
