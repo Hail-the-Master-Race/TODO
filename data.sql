3fffab769080b24bd5e8281e140dc3a3e4469cc0
Jonathan Jin <jjin082693@gmail.com>
2014-04-09 16:18:41 -0500

    add more weapons

--- data.sql
+++ data.sql
@@ -3,4 +3,6 @@ BEGIN TRANSACTION;
 CREATE TABLE weapons(name, valMin, valMax);
 INSERT INTO "weapons" VALUES('Twig',1,5);
 CREATE TABLE armor(name, type, valMin, valMax);
+INSERT INTO "weapons" VALUES('Wok',NULL,NULL);
+INSERT INTO "weapons" VALUES('Staff',NULL,NULL);
 COMMIT;
