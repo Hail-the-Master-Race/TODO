16fee49c20f974936a442d26285d7fa90c0e0264
Jonathan Jin <jjin082693@gmail.com>
2014-04-09 20:00:00 -0500

    add rudimentary welcome screen + UI

---
+++ Assets/Scripts/welcomeUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class welcomeUI : MonoBehaviour
+{
+		void OnGUI ()
+		{
+				GUILayout.Label ("TODO: Dungeon of Doom, Obviously");
+
+				if (GUILayout.Button ("New Game")) {
+						Debug.Log ("TODO: begin new game");
+				}
+
+				if (GUILayout.Button ("Continue")) {
+						Debug.Log ("TODO: continue game");
+				}
+
+				if (GUILayout.Button ("Settings")) {
+						Debug.Log ("TODO: settings");
+				}
+
+				if (GUILayout.Button ("High Score\nand\nAchievements")) {
+						Debug.Log ("TODO: high score and achievements");
+				}
+
+		}
+}
