996b0a0aba0edb1659373d1a68cc615702dd639b
Jonathan Jin <jjin082693@gmail.com>
2014-04-11 18:48:12 -0500

    add scripts for character creation

---
+++ Assets/Scripts/CharacterCreate/PCParameterScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PCParameterScript : MonoBehaviour
+{
+
+    UILabel ClassDescriptorLabel;
+    
+    void Start ()
+    {
+        ClassDescriptorLabel = GameObject.Find ("Label: Class Descriptor")
+            .GetComponent<UILabel> ();
+    }
+
+    public void ClassSelectionChange ()
+    {
+        // TODO
+        string ClassSelection = GameObject.Find ("List: Class Select")
+            .GetComponent<UIPopupList> ().value;
+        ClassDescriptorLabel.text = "Descriptor of " + ClassSelection;
+    }
+}
