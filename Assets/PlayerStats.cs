47e7464f342277fa0ed862466bfd3a7b025627f7
Jonathan Jin <jjin082693@gmail.com>
2014-04-11 11:28:01 -0500

    add script to hold PC stats

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour
+{
+    private int lvl;
+    public int HP;
+    public int MP;
+
+    public string name;
+
+    // Use this for initialization
+    void Start ()
+    {
+        HP = 100;
+        MP = 100;
+
+        name = "PC Placeholder Name";
+    }
+
+    public int getLevel ()
+    {
+        return lvl;
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+	
+    }
+}
