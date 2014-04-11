85b6e56f3373b0bf7fb781f4e073a06443c647e6
Jonathan Jin <jjin082693@gmail.com>
2014-04-11 17:50:22 -0500

    add scripts for Welcome Scene

---
+++ Assets/Scripts/Welcome/SettingsButtonClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsButtonClick : MonoBehaviour
+{
+
+    public void OnClick ()
+    {
+        Application.LoadLevel ("SettingScene");
+    }
+}
