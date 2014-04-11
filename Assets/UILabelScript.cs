161255bdd67949af60341bcac487dfa6ba722cc7
Jonathan Jin <jjin082693@gmail.com>
2014-04-11 11:23:43 -0500

    add UI label for PC info

---
+++ Assets/UILabelScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UILabelScript : MonoBehaviour
+{
+    GameObject PC;
+    UILabel labelPCName;
+    int t = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        PC = GameObject.FindGameObjectWithTag ("Player");
+        labelPCName = GetComponent<UILabel> ();
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        labelPCName.text = PC.GetComponent<PlayerController> ().name;
+        labelPCName.text += (" Lvl." + PC.GetComponent<PlayerController> ().getLevel ());
+    }
+}
