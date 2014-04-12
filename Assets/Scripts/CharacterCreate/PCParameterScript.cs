93dbebce3445b0247796ee55c74a85214b8e319b
Jonathan Jin <jjin082693@gmail.com>
2014-04-11 22:43:50 -0500

    rename hierarchy objects for consistency and clarity

--- Assets/Scripts/CharacterCreate/PCParameterScript.cs
+++ Assets/Scripts/CharacterCreate/PCParameterScript.cs
@@ -8,14 +8,14 @@ public class PCParameterScript : MonoBehaviour
     
     void Start ()
     {
-        ClassDescriptorLabel = GameObject.Find ("Label: Class Descriptor")
+        ClassDescriptorLabel = GameObject.Find ("Class Descriptor")
             .GetComponent<UILabel> ();
     }
 
     public void ClassSelectionChange ()
     {
         // TODO
-        string ClassSelection = GameObject.Find ("List: Class Select")
+        string ClassSelection = GameObject.Find ("Class Select: List")
             .GetComponent<UIPopupList> ().value;
         ClassDescriptorLabel.text = "Descriptor of " + ClassSelection;
     }
