f3f7bff82f22abb0467d3d3bf01b6d0880401b27
Syuurinn <allisonkramer@uchicago.edu>
2014-04-15 10:53:28 -0500

    3D movement, -gravity

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,46 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour
-{
+// Require a character controller to be attached to the same game object
+[RequireComponent(typeof (CharacterController))]
+[AddComponentMenu("Third Person Player/Third Person Controller")]
 
-    public float pos;
-    Animator playerAnimator;
-    float speed = 0f;
-    float direction = 0f;
+public class PlayerController : MonoBehaviour {
 	
-    void Start ()
-    {
-        speed = 0f;
-        playerAnimator = GetComponent<Animator> ();
-    }
+	public float rotationDamping = 0.5f;
+	public float pos;
+	public float runSpeed = 0f;
+	public int gravity = 20;
+	public float jumpSpeed = 0f;
+	float speed = 0f;
 
-    void AddSpeed (float delta)
-    {
-        speed = Mathf.Clamp (speed + delta, -1f, 1f);
-        playerAnimator.SetFloat ("Speed", speed);
-    }
+	private Animator playerAnimator;
 	
-    void SetSpeed (float spd)
-    {
-        speed = spd;
-        playerAnimator.SetFloat ("Speed", speed);
-    }
+	bool canJump;
+	float moveSpeed;
+	float verticalVel; // Used for continuing momentum while in air
+	CharacterController controller;
 	
-    void UpdateMovement ()
-    {
-        direction = Input.GetAxis ("Horizontal");
-
-        if (direction != 0)
-            AddSpeed (direction * 0.01f);
-        else
-            SetSpeed (0f);
+	void Start()
+	{
+		controller = (CharacterController)GetComponent(typeof(CharacterController));
+		playerAnimator = GetComponent<Animator>();
+	}
+	void AddSpeed (float delta)
+	{
+		speed = Mathf.Clamp (speed + delta, -1f, 1f);
+		playerAnimator.SetFloat ("Speed", speed);
+	}
+	
+	void SetSpeed (float spd)
+	{
+		speed = spd;
+		playerAnimator.SetFloat ("Speed", speed);
+	}
+	float UpdateMovement()
+	{
+		// Movement
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
 		
-        pos += speed * Time.deltaTime;
-    }
-	
-    void Update ()
-    {
-        UpdateMovement ();
-    }
+		Vector3 inputVec = new Vector3(x, 0, z);
+		inputVec *= runSpeed;
+		
+		//controller.Move((inputVec + Vector3.up * -gravity + new Vector3(0, verticalVel, 0)) * Time.deltaTime);
+		
+		// Rotation
+		if (inputVec != Vector3.zero)
+			transform.rotation = Quaternion.Slerp(transform.rotation,
+			                                      Quaternion.LookRotation(inputVec),
+			                                      Time.deltaTime * rotationDamping);
+
+		if (Mathf.Abs(x) + Mathf.Abs(z) != 0)
+			AddSpeed (((Mathf.Abs(x) + (Mathf.Abs(z)))) * 0.01f);
+		else
+			SetSpeed (0f);
+		
+		pos += speed * Time.deltaTime;
+
+		return inputVec.magnitude;
+	}
+	void Update()
+	{
+		// Check for jump
+		if (controller.isGrounded )
+		{
+			canJump = true;
+			if ( canJump && Input.GetKeyDown("space") )
+			{
+				// Apply the current movement to launch velocity
+				verticalVel = jumpSpeed;
+				canJump = false;
+			}
+		}else
+		{
+			// Apply gravity to our velocity to diminish it over time
+			verticalVel += Physics.gravity.y * Time.deltaTime;
+		}
+		
+		// Actually move the character
+		moveSpeed = UpdateMovement();
+
+		//AddSpeed (moveSpeed * 0.01f);
+
+		if ( controller.isGrounded )
+			verticalVel = 0f;// Remove any persistent velocity after landing
+	}
 }
