dc334fbf897ef0e3b5ba38708ce65ee95857f73e
Syuurinn <allisonkramer@uchicago.edu>
2014-04-20 14:28:09 -0500

    Clean up controller code

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -7,21 +7,20 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	public float turnSmoothing = 15f;   // A smoothing value for turning the player.
+	public float turnSmoothing = 15f;   // A smoothing value for turning the player
 	public float speedDampTime = 0.1f;  // The damping for the speed parameter
 	
 	
-	private Animator playerAnimator;    	// Reference to the animator component.
-	private HashIDs hash;               	// Reference to the HashIDs.
-	private CharacterController controller; // Reference to player's character controller.
+	private Animator playerAnimator;    	// Player's animator
+	private HashIDs hash;               	// HashIDs for GameController
+	private CharacterController controller; // Player's character controller
 	private Camera mainCamera;
 
-	private float verticalVel = 0f;			// Player's vertical velocity.
+	private float verticalVel = 0f;			// Player's vertical velocity
 	
 	
 	void Start ()
 	{
-		// Setting up the references.
 		playerAnimator = GetComponent<Animator>();
 		controller = (CharacterController)GetComponent (typeof(CharacterController));
 		hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<HashIDs>();
@@ -30,65 +29,66 @@ public class PlayerController : MonoBehaviour
 	
 	void Update ()
 	{
-		//will probably need later
+		// Will probably need later for sounds and stuff
 	}
 	
 	void FixedUpdate ()
 	{
-		// Cache the inputs.
+		// Cache the inputs
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
-		
-		UpdateMovement(h, v);
+		float rot = Input.GetAxis ("Mouse X");
+
+		UpdateMovement (h, v, rot);
 	}
 	
-	void UpdateMovement (float horizontal, float vertical)
+	void UpdateMovement (float horizontal, float vertical, float rotation)
 	{
+		// Jumping will be implemented here
 		if (controller.isGrounded) {
 			Debug.Log ("On the ground!");
 		} else {
 			verticalVel += Physics.gravity.y * Time.deltaTime;
+			return;
 		}
-		// If there is some axis input...
-		if(horizontal != 0f || vertical != 0f)
+
+		// Check for forward/backward motion
+		if(vertical != 0f)
 		{
-			// ... set the players rotation and set the speed parameter to 5.5f.
-			if(vertical >= 0) {
+			// We are moving forward
+			if(vertical > 0) {
 				playerAnimator.SetFloat(hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
 			}
-			else if (vertical < 0) {
+			else { // We are moving backward
 				playerAnimator.SetFloat(hash.speedFloat, -5.5f, speedDampTime, Time.deltaTime);
 			}
 		}
 		else
-			// Otherwise set the speed parameter to 0.
+			// Otherwise set the speed parameter to 0
 			playerAnimator.SetFloat(hash.speedFloat, 0);
 
-		float x = Input.GetAxis ("Mouse X");
-		Rotate (x, 0);
+		// Roate based on mouse, independent of movement
+		Rotate (rotation);
 	}
 	
 	
-	void Rotate (float horizontal, float vertical)
+	void Rotate (float rotation)
 	{
-		if (vertical == 0)
-			vertical += 1;
-
 		Vector3 forward = mainCamera.transform.forward;
 		Vector3 right = mainCamera.transform.right;
 
-		// Create a new vector of the horizontal and vertical inputs.
-		Vector3 targetDirection = horizontal * right + vertical * forward;
+		// Direction depends on the camera
+		Vector3 targetDirection = rotation * right + forward;
 		targetDirection.y = 0f;
 		
-		// Create a rotation based on this new vector assuming that up is the global y axis.
+		// Create the rotation
 		if (targetDirection != Vector3.zero) {
 			Quaternion targetRotation = Quaternion.LookRotation (targetDirection);
 		
-			// Create a rotation that is an increment closer to the target rotation from the player's rotation.
+			// Create a rotation that is an increment closer to the target rotation from the player's rotation
 			Quaternion newRotation = Quaternion.Lerp (rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
 		
-			// Change the players rotation to this new rotation.
+			// Change the players rotation to this new rotation
 			rigidbody.MoveRotation (newRotation);
 		}
 	}
