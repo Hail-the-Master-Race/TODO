cb1f415f3a087180248cc2a9447395d7cfcaa0d7
Jonathan Jin <jjin082693@gmail.com>
2014-05-07 14:21:35 -0500

    make Character class inherit from MonoBehavior

---
+++ Assets/Scripts/CharacterCreate/Character.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Character : MonoBehaviour
+{       
+    public string Name;
+    private Dictionary<statIndex, int> StartingStats;
+    
+    private const int totalStatPoints = 15;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Name = "";
+        InitStats ();
+    }
+
+    private void InitStats ()
+    {
+        if (StartingStats == null)
+            StartingStats = new Dictionary<statIndex, int> ();
+        
+        if (!StartingStats.ContainsKey (statIndex.STR))
+            StartingStats.Add (statIndex.STR, 0);
+        if (!StartingStats.ContainsKey (statIndex.FRT))
+            StartingStats.Add (statIndex.FRT, 0);
+        if (!StartingStats.ContainsKey (statIndex.DEX))
+            StartingStats.Add (statIndex.DEX, 0);
+        
+        if (StartingStats [statIndex.STR] != 0) 
+            StartingStats [statIndex.STR] = 0;
+        if (StartingStats [statIndex.FRT] != 0)
+            StartingStats [statIndex.FRT] = 0;
+        if (StartingStats [statIndex.DEX] != 0)
+            StartingStats [statIndex.DEX] = 0;
+    }
+
+    public void RerollStats ()
+    {
+        InitStats ();
+        
+        for (int i=0; i< totalStatPoints; i++) {
+            StartingStats [(statIndex)Random.Range (0, (int)statIndex.MAX)]++;
+        }     
+        return;
+    }
+    
+    public int getStartingStat (statIndex index)
+    {
+        return StartingStats [index];
+    }
+    
+    public void setName (string name)
+    {
+        Name = name;
+    }
+}
--- Assets/Scripts/CharacterCreate/CharacterCreateScript.cs
+++ Assets/Scripts/CharacterCreate/CharacterCreateScript.cs
@@ -10,78 +10,14 @@ public enum statIndex
     MAX
 }
 
-public class Character
-{
-    private string Name;
-    private Dictionary<statIndex, int> StartingStats;
-
-    private const int totalStatPoints = 15;
-    
-    public Character ()
-    {
-        Name = "";
-       
-        StartingStats = new Dictionary<statIndex, int> ();
-
-        StartingStats.Add (statIndex.STR, 0);
-        StartingStats.Add (statIndex.FRT, 0);
-        StartingStats.Add (statIndex.DEX, 0);
-
-        StartingStats [statIndex.STR] = 0;
-        StartingStats [statIndex.FRT] = 0;
-        StartingStats [statIndex.DEX] = 0;
-        
-    }
-    
-    private void InitStats ()
-    {
-        if (StartingStats == null)
-            StartingStats = new Dictionary<statIndex, int> ();
-        
-        if (!StartingStats.ContainsKey (statIndex.STR))
-            StartingStats.Add (statIndex.STR, 0);
-        if (!StartingStats.ContainsKey (statIndex.FRT))
-            StartingStats.Add (statIndex.FRT, 0);
-        if (!StartingStats.ContainsKey (statIndex.DEX))
-            StartingStats.Add (statIndex.DEX, 0);
-        
-        if (StartingStats [statIndex.STR] != 0) 
-            StartingStats [statIndex.STR] = 0;
-        if (StartingStats [statIndex.FRT] != 0)
-            StartingStats [statIndex.FRT] = 0;
-        if (StartingStats [statIndex.DEX] != 0)
-            StartingStats [statIndex.DEX] = 0;
-    }
-    
-    public void RerollStats ()
-    {
-        InitStats ();
-        
-        for (int i=0; i< totalStatPoints; i++) {
-            StartingStats [(statIndex)Random.Range (0, (int)statIndex.MAX)]++;
-        }     
-        return;
-    }
-    
-    public int getStartingStat (statIndex index)
-    {
-        return StartingStats [index];
-    }
-
-    public void setName (string name)
-    {
-        Name = name;
-    }
-}
-
 public class CharacterCreateScript : MonoBehaviour
 {
     private UIController UI;
-
-    private Character PC = new Character ();
+    private Character PC;
 
     public void Start ()
     {
+        PC = GetComponent<Character> ();
         InitClasses ();
         RerollStats ();
     }
@@ -109,9 +45,10 @@ public class CharacterCreateScript : MonoBehaviour
 
     public void RerollStats ()
     {
+        PC = GetComponent<Character> ();
         PC.RerollStats ();
-        UI = GetComponent<UIController> ();
 
+        UI = GetComponent<UIController> ();
         UI.UpdateStatTable (PC.getStartingStat (statIndex.STR),
                             PC.getStartingStat (statIndex.DEX),
                             PC.getStartingStat (statIndex.FRT));
