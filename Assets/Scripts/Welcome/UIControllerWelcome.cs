ebe689245eb196768884871a8f0872043cec3dd6
Jonathan Jin <jjin082693@gmail.com>
2014-05-08 16:34:51 -0500

    Welcome: wire up new game + achievements buttons

---
+++ Assets/Scripts/Welcome/UIControllerWelcome.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControllerWelcome : MonoBehaviour
+{
+
+    public void OnClickNewGame ()
+    {
+        Application.LoadLevel ("CharacterCreateScene");
+    }
+
+    public void OnClickHighScore ()
+    { 
+        Application.LoadLevel ("HighScoreAchievementsScene");
+    }
+
+    public void OnClickSettings ()
+    {
+        Application.LoadLevel ("SettingScene");
+    }
+}
