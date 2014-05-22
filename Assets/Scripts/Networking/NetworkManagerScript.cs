92c2a9de4525971bf882006c7e5845f8798cdcbd
Alexander Bake <abake@uchicago.edu>
2014-05-21 21:22:02 -0500

    Added extremely basic pause functionality for networking. Should really move this to something else though...

--- Assets/Scripts/Networking/NetworkManagerScript.cs
+++ Assets/Scripts/Networking/NetworkManagerScript.cs
@@ -5,6 +5,8 @@ public class NetworkManagerScript : MonoBehaviour {
 	public GameObject playerPrefab;
 	public Transform spawnObject;
 
+	// Janky janky janky...
+	private bool isPaused = false;
 	private bool refreshing;
 	private HostData[] hostData = null;
 
@@ -22,14 +24,24 @@ public class NetworkManagerScript : MonoBehaviour {
 		btnW = Screen.width  * (float) 0.15;
 		btnH = Screen.width  * (float) 0.15;
 	}
+
+	void UpdateIsPasued () {
+		isPaused = !isPaused;
+		// Stop time when paused
+		Time.timeScale = isPaused ? 0 : 1;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (refreshing) {
-			if (MasterServer.PollHostList ().Length > 0) {
-				refreshing = false;
-				Debug.Log (MasterServer.PollHostList ().Length);
-				hostData = MasterServer.PollHostList ();
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			UpdateIsPasued();
+
+			if (refreshing) {
+				if (MasterServer.PollHostList ().Length > 0) {
+					refreshing = false;
+					Debug.Log (MasterServer.PollHostList ().Length);
+					hostData = MasterServer.PollHostList ();
+				}
 			}
 		}
 	}
@@ -76,25 +88,28 @@ public class NetworkManagerScript : MonoBehaviour {
 // -- This will need to be changed to use NGUI and only appear on pause menu
 // -- Shouldn't be that hard...
 	void OnGUI () {	
-		if (!Network.isClient && !Network.isServer) {
-			if (GUI.Button (new Rect (btnX, btnY, btnW, btnH), "Start Server")) {
-				Debug.Log ("Starting server");
-				startServer ();
-			}	
+		if (isPaused) {
+			if (!Network.isClient && !Network.isServer) {
+				if (GUI.Button (new Rect (btnX, btnY, btnW, btnH), "Start Server")) {
+					Debug.Log ("Starting server");
+					startServer ();
+					UpdateIsPasued();
+				}	
 
-			if (GUI.Button (new Rect (btnX, btnY * (float)2.5, btnW, btnH), "Refresh Hosts")) {
-				Debug.Log ("Refreshing hosts list");
-				refreshHostList ();
-			}
-			if (hostData != null) {
-				for (int i = 0; i < hostData.Length; ++i) {
-					if (GUI.Button (new Rect (
-						btnX * (float)1.5 + btnW, 
-	     				btnY * (float)2.5 + (btnH * i),
-	     				btnW * (float)3,
-	     				btnH * (float)0.25),
-						hostData [i].gameName)) {
-							Network.Connect (hostData [i]);
+				if (GUI.Button (new Rect (btnX, btnY * (float)2.5, btnW, btnH), "Refresh Hosts")) {
+					Debug.Log ("Refreshing hosts list");
+					refreshHostList ();
+				}
+				if (hostData != null) {
+					for (int i = 0; i < hostData.Length; ++i) {
+						if (GUI.Button (new Rect (
+							btnX * (float)1.5 + btnW, 
+							btnY * (float)2.5 + (btnH * i),
+							btnW * (float)3,
+							btnH * (float)0.25),
+							hostData [i].gameName)) {
+								Network.Connect (hostData [i]);
+						}
 					}
 				}
 			}
