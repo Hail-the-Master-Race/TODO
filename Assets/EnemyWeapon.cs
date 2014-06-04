45d8794b6757d08b6914aa0e9ffae0da63667fa1
Syuurinn <allisonkramer@uchicago.edu>
2014-06-04 15:30:55 -0500

    Enemy animations and attacks what what

---
+++ Assets/EnemyWeapon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWeapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Enemy Collision");
+		if (other.gameObject.CompareTag ("Player")) {
+			Player player = other.gameObject.transform.root.GetComponent<Player> ();
+			EnemyStats enemyStats = this.gameObject.transform.root.GetComponent<EnemyStats> ();
+
+			player.stats.currentHP -= enemyStats.damage;
+		}
+	}
+}
--- Assets/Scripts/Gameplay/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemy.cs
@@ -11,12 +11,12 @@ public class Enemy : MonoBehaviour
 	public bool attacking = false;
 
 	public void Start() {
-		enemyAnimator = GetComponent<Animator> ();
+		//enemyAnimator = GetComponent<Animator> ();
 		hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
 	}
 
 	public void Update() {
-		enemyAnimator.SetBool (hash.attackBool, true);
+		//enemyAnimator.SetBool (hash.attackBool, true);
 		attacking = true;
 	}
 }
